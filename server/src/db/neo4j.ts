9fa3824efd104cc2bb5d7227b4867e1898c28c9e
cmcaboy <cory.mcaboy@gmail.com>
2018-07-14 08:09:23 -0400

    Added new neo4j instance

--- server/src/db/neo4j.ts
+++ server/src/db/neo4j.ts
@@ -1,4 +1,4 @@
-import {NEO4J_USERNAME,NEO4J_PASSWORD,NEO4J_URL} from '../sensitive';
+import {NEO4J_USER,NEO4J_PASSWORD,NEO4J_URL} from '../sensitive';
 const neo4j = require('neo4j-driver').v1;
 
 const driver = neo4j.driver(NEO4J_URL,neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD));
