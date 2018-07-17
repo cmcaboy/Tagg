c95afef6ada547882597ac7dd0cb961f335a2702
cmcaboy <cory.mcaboy@gmail.com>
2018-07-17 06:54:08 -0400

    dry run

--- server/src/schema/resolvers.ts
+++ server/src/schema/resolvers.ts
@@ -472,6 +472,15 @@ const resolvers = {
                     })
                     .catch(e => console.log('bid error: ',e))
         },
+        num_bids: (parentValue, _) => {
+            return session
+                .run(`MATCH(d:Date{id:'${parentValue.id}'})
+                    WITH size((d)<-[:BID]-(:User)) as num_bids
+                    return num_bids`)
+                    .then(result => result.records[0])
+                    .then(record => record._fields[0])
+                    .catch(e => console.log('num_bids error: ',e))
+        },
     },
     Match: {
         messages: async (parentValue, args) => {
@@ -740,16 +749,16 @@ const resolvers = {
             // Update neo4j values
             try {
                 const data = await session.run(`MATCH (a:User{id:'${id}'})-[:CREATE]->(d:Date{id:'${dateId}'})<-[r:BID]-(b:User{id:'${winnerId}'}) 
-                    WITH d,a,b,r, size((d)<-[:BID]-(:User)) as num_bids
+                    WITH d,a,b,r
                     SET r.winner=TRUE,
                     d.winner='${winnerId}',
                     d.open=FALSE
-                    return d,a,b,num_bids`)
+                    return d,a,b`)
                 date = {
                     ...data.records[0]._fields[0].properties,
                     creator: data.records[0]._fields[1].properties,
                     winner: data.records[0]._fields[2].properties,
-                    num_bids: data.records[0]._fields[3],
+                    //num_bids: data.records[0]._fields[3],
                 }
 
             } catch(e) {
