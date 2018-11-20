d39afcc7f302ed677fbecc32a7760be143108181
Jonathan McAboy <jonathanmcaboy@Jonathans-MacBook-Pro.local>
2018-11-20 11:38:31 -0600

    fixed typo with importing .graphql file

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("apollo-server-express");
 const { importSchema } = require("graphql-import");
-const typeDefs = importSchema("typeDefs.graphql");
+const typeDefs = importSchema("./typeDefs.graphql");
 
 //import typeDefs from "./typeDefs";
 import { resolvers } from "./resolvers";
