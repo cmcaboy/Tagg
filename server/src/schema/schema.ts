3986957e96e56db25c945b2a02734e12f3fd2211
Jonathan McAboy <jonathanmcaboy@Jonathans-MacBook-Pro.local>
2018-11-20 12:05:43 -0600

    rolled back to previous use of schema tracking

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -1,8 +1,8 @@
 const { ApolloServer } = require("apollo-server-express");
-const { importSchema } = require("graphql-import");
-const typeDefs = importSchema("./typeDefs.graphql");
+// const { importSchema } = require("graphql-import");
+// const typeDefs = importSchema("./typeDefs.graphql");
 
-//import typeDefs from "./typeDefs";
+import typeDefs from "./typeDefs";
 import { resolvers } from "./resolvers";
 
 export default new ApolloServer({
