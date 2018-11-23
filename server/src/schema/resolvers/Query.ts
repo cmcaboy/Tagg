b00e3ae604ed280848e4b5507cfc0678ca609069
Jonathan McAboy <jonathanmcaboy@Jonathans-MacBook-Pro.local>
2018-11-23 07:16:29 -0600

    testing auth in user query

--- server/src/schema/resolvers/Query.ts
+++ server/src/schema/resolvers/Query.ts
@@ -1,42 +1,30 @@
-import { QueryResolvers } from "../../types/generated";
+import { QueryResolvers } from '../../types/generated';
 
 export const Query: QueryResolvers.Type = {
   ...QueryResolvers.defaultResolvers,
-  user: async (_, { id, hostId }, { dataSources }) => {
-    return await dataSources.neoAPI.findUser({ id, hostId });
+  user: async (_, { id, hostId }, { dataSources, user }) => {
+    console.log('user: ', user);
+    return await dataSources.neoAPI.findUser({ id: user.id, hostId });
   },
-  messages: async (_, { id }, { datasources }) => {
-    return await datasources.firestoreAPI.getMessages({ id });
-  },
-  date: async (_, { id }, { datasources }) => {
-    return await datasources.neoAPI.findDate({ id });
-  },
+  messages: async (_, { id }, { datasources }) => await datasources.firestoreAPI.getMessages({ id }),
+  date: async (_, { id }, { datasources }) => await datasources.neoAPI.findDate({ id }),
   dates: (_, __) => {
-    throw new Error("Resolver not implemented");
+    throw new Error('Resolver not implemented');
   },
-  otherBids: async (_, { id }, { datasources }) => {
-    // dateID should be passed in as the id
-    // Need to factor in pagination
-    // Should sort by date?
-    return await datasources.neoAPI.findOtherBids({ id });
-  },
-  moreMessages: async (_, { id, cursor }, { datasources }) => {
-    return await datasources.firestoreAPI.getMoreMessages({ id, cursor });
-  },
-  moreQueue: async (_, { id, cursor, followerDisplay }, { datasources }) => {
-    return await datasources.neoAPI.getQueueMore({
-      id,
-      cursor,
-      followerDisplay
-    });
-  },
+  otherBids: async (_, { id }, { datasources }) => await datasources.neoAPI.findOtherBids({ id }),
+  moreMessages: async (_, { id, cursor }, { datasources }) => await datasources.firestoreAPI.getMoreMessages({ id, cursor }),
+  moreQueue: async (_, { id, cursor, followerDisplay }, { datasources }) => await datasources.neoAPI.getQueueMore({
+    id,
+    cursor,
+    followerDisplay,
+  }),
   moreDates: (_, __) => {
-    throw new Error("Resolver not implemented");
+    throw new Error('Resolver not implemented');
   },
   moreDateBids: (_, __) => {
-    throw new Error("Resolver not implemented");
+    throw new Error('Resolver not implemented');
   },
   moreFollowing: (_, __) => {
-    throw new Error("Resolver not implemented");
-  }
+    throw new Error('Resolver not implemented');
+  },
 };
