e83f62b785ab2441fa0291d0ba0b98099daa9b12
Jonathan McAboy <jonathanmcaboy@Jonathans-MBP.fios-router.home>
2018-12-09 16:15:19 -0500

    using dateId on otherBids rather than user.id

--- server/src/schema/resolvers/Query.ts
+++ server/src/schema/resolvers/Query.ts
@@ -21,15 +21,13 @@ export const Query: QueryResolvers.Type = {
   dates: (_, __) => {
     throw new Error('Resolver not implemented');
   },
-  otherBids: async (_, { id: argsId }, { dataSources, user }) => {
+  otherBids: async (_, { id: dateId }, { dataSources, user }) => {
     if (!user || !user.id) {
       console.log('User not authenticated');
       throw new AuthenticationError('User not authenticated');
     }
 
-    const id = argsId || user.id;
-
-    return await dataSources.neoAPI.findOtherBids({ id });
+    return await dataSources.neoAPI.findOtherBids({ id: dateId });
   },
   moreMessages: async (_, { id, cursor }, { dataSources }) => await dataSources.firestoreAPI.getMoreMessages({ id, cursor }),
   moreQueue: async (_, { cursor, followerDisplay }, { dataSources, user }) => {
