8725ea60d93982df617da0103db615edd38c1d40
Jonathan McAboy <jonathanmcaboy@Jonathans-MBP.fios-router.home>
2018-12-09 16:53:00 -0500

    starting fix on otherBids

--- server/src/schema/resolvers/DateBid.ts
+++ server/src/schema/resolvers/DateBid.ts
@@ -4,6 +4,9 @@ export const DateBid: DateBidResolvers.Type = {
   ...DateBidResolvers.defaultResolvers,
 
   dateUser: async ({ id }, _, { dataSources }) => await dataSources.neoAPI.getDateCreator({ id }),
-  bidUser: async ({ id }, _, { dataSources }) => await dataSources.neoAPI.findDateBidder({ id }),
+  bidUser: async ({ id }, _, { dataSources }) => {
+    console.log('bidUser parent id: ', id);
+    return await dataSources.neoAPI.findDateBidder({ id });
+  },
   date: async ({ id }, _, { dataSources }) => await dataSources.neoAPI.findDateFromBid({ id }),
 };
