d94e5691c8c8a6ec01df8b55ee88eeb84d05a362
Jonathan McAboy <jonathanmcaboy@Jonathans-MacBook-Pro.local>
2018-12-29 19:15:59 -0600

    checking for 0 in graphql context

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -45,7 +45,7 @@ const context = async ({ req }: { req: any }) => {
   let neoRaw;
   let user;
 
-  if (!email) {
+  if (!email || email === '0') {
     console.log('No email in header!');
     throw new AuthenticationError('User not authenticated; Please try to logout and login again.');
   }
